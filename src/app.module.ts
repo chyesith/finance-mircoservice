dd01e12a627ab57097dd1fcf8353609038447f9f
Yesith <108515089+yesith94@users.noreply.github.com>
2023-05-07 21:03:38 +0100

    update for account

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountServiceService } from './services/account-service/account-service.service';
+import { InvoiceServiceService } from './services/invoice-service/invoice-service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AccountServiceService, InvoiceServiceService],
 })
 export class AppModule {}
